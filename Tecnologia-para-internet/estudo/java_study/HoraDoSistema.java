a771d636469fe29992111cab1a83a1abd05c1b3c
Ruan Ferraz <ruanpabloferraz007curujinha@gmail.com>
2026-06-20 14:31:45 -0300

    estudando dnv...

---
+++ Tecnologia-para-internet/estudo/java_study/HoraDoSistema.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+public class HoraDoSistema {
+
+    public static void main(String[] args) {
+        Date dataehora = new Date();
+        System.out.print("A hora do sistema é:");
+        System.out.println(dataehora.toString());
+    }
+    
+}
--- Tecnologia-para-internet/estudo/java_study/PrimeiroPrograma.java
+++ Tecnologia-para-internet/estudo/java_study/PrimeiroPrograma.java
@@ -1,6 +1,7 @@
 public class PrimeiroPrograma {
     public static void main(String[] args){
         System.out.println("Ola, mundo!");
+        System.out.print("Como c ta?");
     }
 
 }
