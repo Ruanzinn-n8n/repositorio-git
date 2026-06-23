74ec800b49163936bd3671b30bcd6e4d74f45a4d
Ruan Ferraz <ruanpabloferraz007curujinha@gmail.com>
2026-06-23 01:25:16 -0300

    Update HoraDoSistema.java

--- Tecnologia-para-internet/estudo/java_study/HoraDoSistema.java
+++ Tecnologia-para-internet/estudo/java_study/HoraDoSistema.java
@@ -1,11 +1,16 @@
 import java.util.Date;
+import java.util.Locale;
 
 public class HoraDoSistema {
 
     public static void main(String[] args) {
         Date dataehora = new Date();
-        System.out.print("A hora do sistema é:");
+        Locale language = new Locale();
+
+        System.out.print("A hora do sistema é: ");
         System.out.println(dataehora.toString());
+        System.out.print("Seu sistema está em ");
+        System.out.println(language.toString());
     }
     
 }
